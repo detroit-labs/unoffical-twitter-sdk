4d8f12e11b561ae0419d54a2c916c8e5912c81e8
Jeff Kelley <SlaunchaMan@gmail.com>
2012-03-14 14:14:47 -0400

    Update to avoid modifying the files in a submodule for each project.

--- unoffical-twitter-sdk/OAuth/OAuthConsumerCredentials.h
+++ unoffical-twitter-sdk/OAuth/OAuthConsumerCredentials.h
@@ -1,4 +1,6 @@
-#error Use your Twitter keys from developer.twitter.com
+#ifndef TWITTER_CREDENTIALS_PROVIDED
+	#error Use your Twitter keys from developer.twitter.com
 
-#define kDEConsumerKey @"GetYourOwn"
-#define kDEConsumerSecret @"GetYourOwn"
+	#define kDEConsumerKey @"GetYourOwn"
+	#define kDEConsumerSecret @"GetYourOwn"
+#endif
